b5ded1d8c2a90ec9782723bcb1d8ff48dee5d3ed
Minerr <jack.corp@hotmail.com>
2017-12-16 14:32:03 +0100

    DragObject now works
    Fixed DragObject
    Added texture when howering over draggable objects

--- TimeJam/Assets/Resources/Scripts/DragObject.cs
+++ TimeJam/Assets/Resources/Scripts/DragObject.cs
@@ -5,37 +5,79 @@ using UnityEngine;
 public class DragObject : MonoBehaviour
 {
 	private FixedJoint2D fixedJoint;
-		
+	private bool isOnDraggableObject;
+	private bool canDrag;
+
+	private Collider2D draggableCollider;
+
 	// Use this for initialization
 	void Start ()
 	{
 		fixedJoint = GetComponent<FixedJoint2D>();
 		fixedJoint.enabled = false;
+		isOnDraggableObject = false;
+		canDrag = true;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		if(fixedJoint.connectedBody != null)
+		if(draggableCollider != null)
 		{
 			if(Input.GetKeyDown(KeyCode.E))
 			{
-				fixedJoint.connectedBody = null;
-				fixedJoint.enabled = false;
+				ToggleJoint();
 			}
 		}
 	}
 
-	private void OnCollisionStay2D(Collision2D collision)
+	private void ToggleJoint()
 	{
-		GameObject colliderObject = collision.gameObject;
-		if(colliderObject.tag == "Draggable")
+		if(canDrag)
 		{
-			if(Input.GetKeyDown(KeyCode.E))
-			{
-				fixedJoint.enabled = true;
-				fixedJoint.connectedBody = colliderObject.GetComponent<Rigidbody2D>();
-			}
+			canDrag = false;
+			fixedJoint.enabled = true;
+			fixedJoint.connectedBody = draggableCollider.GetComponent<Rigidbody2D>();
+		}
+		else
+		{
+			canDrag = true;
+			fixedJoint.enabled = false;
+			fixedJoint.connectedBody = null;
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collider)
+	{
+		if(collider.tag == "Draggable")
+		{
+			isOnDraggableObject = true;
+			draggableCollider = collider;
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D collider)
+	{
+		if(collider.tag == "Draggable")
+		{
+			isOnDraggableObject = false;
+			draggableCollider = null;
+		}
+	}
+
+	private void OnGUI()
+	{
+		if(canDrag && isOnDraggableObject)
+		{
+			Texture tex_clickToDrag = Resources.Load<Texture>("Textures/Tex_ClickToDrag");
+
+			float imageScale = 3;
+			float imageWidth = tex_clickToDrag.width * imageScale;
+			float imageHeight = tex_clickToDrag.height * imageScale;
+			float imageCenterX = imageWidth / 2;
+			float imageCenterY = 100 + (imageHeight / 2);
+
+			GUI.DrawTexture(new Rect((Screen.width / 2) - imageCenterX, (Screen.height / 2) - imageCenterY, imageWidth, imageHeight), tex_clickToDrag, ScaleMode.ScaleToFit, true, 10.0F);
 		}
 	}
 }
