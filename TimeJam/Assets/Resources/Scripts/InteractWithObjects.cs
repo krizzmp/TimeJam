734fd5a619fc36fef474760fd05d460365522312
Minerr <jack.corp@hotmail.com>
2017-12-16 18:25:24 +0100

    Puck up item & inventory

--- TimeJam/Assets/Resources/Scripts/InteractWithObjects.cs
+++ TimeJam/Assets/Resources/Scripts/InteractWithObjects.cs
@@ -6,24 +6,73 @@ public class InteractWithObjects : MonoBehaviour {
 
 	private const int MAX_INVENTORY_SIZE = 6;
 	private Texture[] inventory;
-
+	private bool showPickUpLine;
+	private Collider2D colliderItem;
 
 	// Use this for initialization
 	void Start ()
 	{
+		showPickUpLine = false;
 		inventory = new Texture[MAX_INVENTORY_SIZE];
-		inventory[0] = Resources.Load<Texture>("Textures/Tex_InventoryItem_Sword");
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		
+		if(colliderItem != null)
+		{
+			if(Input.GetKeyDown(KeyCode.E))
+			{
+				Item itemScript = colliderItem.GetComponent<Item>();
+				AddItemToInventory(itemScript.InventoryTexture);
+				Destroy(colliderItem.gameObject);
+				colliderItem = null;
+			}
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collider)
+	{
+		if(collider.tag == "Item")
+		{
+			showPickUpLine = true;
+			colliderItem = collider;
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D collider)
+	{
+		if(collider.tag == "Item")
+		{
+			showPickUpLine = false;
+			colliderItem = null;
+		}
 	}
 
 	private void OnGUI()
 	{
 		DrawInventory();
+
+		if(showPickUpLine)
+		{
+			DrawPickUpLine();
+		}
+	}
+
+	private void DrawPickUpLine()
+	{
+		if(showPickUpLine)
+		{
+			Texture tex_pressToPickUp = Resources.Load<Texture>("Textures/Tex_PressToPickUp");
+
+			float imageScale = 3;
+			float imageWidth = tex_pressToPickUp.width * imageScale;
+			float imageHeight = tex_pressToPickUp.height * imageScale;
+			float imageCenterX = imageWidth / 2;
+			float imageCenterY = 100 + (imageHeight / 2);
+
+			GUI.DrawTexture(new Rect((Screen.width / 2) - imageCenterX, (Screen.height / 2) - imageCenterY, imageWidth, imageHeight), tex_pressToPickUp, ScaleMode.StretchToFill, true, 10.0F);
+		}
 	}
 
 	private void DrawInventory()
