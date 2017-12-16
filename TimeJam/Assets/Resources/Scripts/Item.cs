7d6bcb5d5c7ad5e3d0f415ef197f96edde014f02
Cem Calgici <Cemox35@live.dk>
2017-12-16 22:01:55 +0100

    Revert "g"
    This reverts commit 6c3623e9a5ac0e6879e1b8b5fb7dd4561ff29ff7.

--- TimeJam/Assets/Resources/Scripts/Item.cs
+++ TimeJam/Assets/Resources/Scripts/Item.cs
@@ -9,11 +9,12 @@ public class Item : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
-        
-    }
+		
+	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-    }
+		
+	}
 }
