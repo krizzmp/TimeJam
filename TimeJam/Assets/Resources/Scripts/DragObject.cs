80d6a86e4b5ea57a0d3607d4972515a3e6a2919b
Minerr <jack.corp@hotmail.com>
2017-12-16 02:56:46 +0100

    Failed at dragging object
    Set player tag on player.
    Added draggable tag
    Added DragObject script that doesn't work yet.

---
+++ TimeJam/Assets/Resources/Scripts/DragObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObject : MonoBehaviour
+{
+	private FixedJoint2D fixedJoint;
+		
+	// Use this for initialization
+	void Start ()
+	{
+		fixedJoint = GetComponent<FixedJoint2D>();
+		fixedJoint.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(fixedJoint.connectedBody != null)
+		{
+			if(Input.GetKeyDown(KeyCode.E))
+			{
+				fixedJoint.connectedBody = null;
+				fixedJoint.enabled = false;
+			}
+		}
+	}
+
+	private void OnCollisionStay2D(Collision2D collision)
+	{
+		GameObject colliderObject = collision.gameObject;
+		if(colliderObject.tag == "Draggable")
+		{
+			if(Input.GetKeyDown(KeyCode.E))
+			{
+				fixedJoint.enabled = true;
+				fixedJoint.connectedBody = colliderObject.GetComponent<Rigidbody2D>();
+			}
+		}
+	}
+}
